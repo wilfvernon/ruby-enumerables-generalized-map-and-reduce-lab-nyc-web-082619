e2b5c3bee0d8e4c59d1e83309bb5d24b04be1a1c
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-22 01:33:28 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
-# Your Code Here
+def map(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push(yield(source_array[i]))
+    i += 1
+  end
+  return new
+end
+
+def reduce(source_array, starting_value = 0)
+  i = 0
+  while i < source_array.length do
+    starting_value = yield(source_array[i], starting_value)
+    i += 1
+end
+if starting_value
+  return starting_value
+else return false
+end
+end
